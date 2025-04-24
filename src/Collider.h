4384cfc02ecf67ce49137d9b4f2204d1c94e31a8
Tifanie Ngo <tifngo457@gmail.com>
2025-04-23 23:06:29 -0700

    added collidier.h comments

--- src/Collider.h
+++ src/Collider.h
@@ -2,7 +2,9 @@
 /// \file         Collider.h
 /// \author       Nathan Teh
 /// \date         4/20/25
-/// \brief        This file sets up the collider for Yo-Ho Jump!
+/// \brief        This file sets up the collider for Yo-Ho Jump! Includes basic 
+///               AABB (Axis-Aligned Bounding Box) collision detection and resolution 
+///               logic
 ///////////////////////////////////////////////////////////////////////////////
 
 #ifndef COLLIDER_H
@@ -11,15 +13,15 @@
 
 class Collider {
 public:
-    Collider(sf::RectangleShape& body) : body(body) {};
-    ~Collider() = default;
-    void Move(float dx, float dy) {body.move(sf::Vector2f(dx, dy));}
-    bool checkCollision(Collider& other, sf::Vector2f& direction,float push);
-    sf::Vector2f GetPosition() {return body.getPosition();}
-    sf::Vector2f GetHalfSize() {return body.getSize()/2.0f;}
+    Collider(sf::RectangleShape& body) : body(body) {}; // collider initialized with reference to rectangle shape
+    ~Collider() = default; // destructor
+    void Move(float dx, float dy) {body.move(sf::Vector2f(dx, dy));} // moves rectangle shape by delta (in x or y direction)
+    bool checkCollision(Collider& other, sf::Vector2f& direction,float push); // checks collision with another collider and returns collision direction
+    sf::Vector2f GetPosition() {return body.getPosition();} // gets position of rectangle shape
+    sf::Vector2f GetHalfSize() {return body.getSize()/2.0f;} // gets half size of rectangle shape
 
 private:
-    sf::RectangleShape& body;
+    sf::RectangleShape& body; // reference to rectangle shape this collider manages
 };
 
 
